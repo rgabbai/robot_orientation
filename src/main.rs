c1fc7863e033e3859e9461022af07610c458e241
rgabbai <rony.gabbai@gmail.com>
2023-11-12 14:29:25 +0200

    Yaw is 0 to 359

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ async fn main() -> Result<()>  {
                data: serialized_data,
             };
             //println!("Publishing: '{}'", message.data);
-            rclrust_info!(logger, "Publishing: '{}'", message.data);
+            //rclrust_info!(logger, "Publishing: '{}'", message.data);
             publisher.publish(&message).unwrap();
         },
         &QoSProfile::default(),
@@ -69,9 +69,44 @@ async fn main() -> Result<()>  {
     Ok(())  
 }
 
+fn quaternion_to_euler(w: f64, x: f64, y: f64, z: f64) -> (f64, f64, f64) {
+    let t0 = 2.0 * (w * x + y * z);
+    let t1 = 1.0 - 2.0 * (x * x + y * y);
+    let roll = t0.atan2(t1);
+
+    let t2 = 2.0 * (w * y - z * x);
+    let t2 = if t2 > 1.0 { 1.0 } else { t2 };
+    let t2 = if t2 < -1.0 { -1.0 } else { t2 };
+    let pitch = t2.asin();
+
+    let t3 = 2.0 * (w * z + x * y);
+    let t4 = 1.0 - 2.0 * (y * y + z * z);
+    let yaw = t3.atan2(t4);
+
+    (roll.to_degrees(), pitch.to_degrees(), yaw.to_degrees())
+}
+
+
+fn convert_to_360(yaw_angle: f64) -> f64 {
+    if yaw_angle < 0.0 {
+        yaw_angle + 360.0
+    } else {
+        yaw_angle
+    }
+}
+
 fn process_imu_data(msg: &Imu_) -> DataOrien {
     // Extract the quaternion from the IMU message
     let q = Quaternion::new(msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z);
+
+    //let w = msg.orientation.w;
+    //let x = msg.orientation.x;
+    //let y = msg.orientation.y;
+    //let z = msg.orientation.z;
+    //let (roll, pitch, yaw) = quaternion_to_euler(w, x, y, z);
+
+    //let yaw_360 = (convert_to_360(yaw)).round();  
+
 
     // Normalize the quaternion
     let norm_quat = UnitQuaternion::from_quaternion(q.normalize());
@@ -80,14 +115,15 @@ fn process_imu_data(msg: &Imu_) -> DataOrien {
     let euler_angles = norm_quat.euler_angles();
 
     // Convert to degrees according to camera Axis
-    let pitch_deg = euler_angles.0.to_degrees();      // X left
-    let roll_deg = euler_angles.1.to_degrees();    // Z forward
-    let yaw_deg = euler_angles.2.to_degrees();      // Y axis down
+    let pitch_deg   = euler_angles.0.to_degrees().round();      // X left
+    let roll_deg    = euler_angles.1.to_degrees().round();    // Z forward
+    let yaw_deg     =  (euler_angles.2.to_degrees()).round();      // Y axis down
+    let yaw_360_deg = (convert_to_360(yaw_deg)).round(); 
 
     // Create and return the Float64MultiArray message
       // Create and return the DataOrien instance
     DataOrien {
-        yaw: yaw_deg as f32,
+        yaw: yaw_360_deg as f32,
         pitch: pitch_deg as f32,
         roll: roll_deg as f32,
     }
